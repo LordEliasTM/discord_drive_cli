2be25931e79238fec0d4b8e8d55d3daeddc95cd5
LordElias <kerlinelias@gmail.com>
2024-01-29 00:21:32 +0100

    tim made gooder types

--- lib/types.dart
+++ lib/types.dart
@@ -1,17 +1,41 @@
 class FolderIndex {
-  int version;            // uint8      Version
-  int lastEditTimestamp;  // uint64     Last edit timestamp
-  int? next;              // uint64     Next
-  List<Entry> entries;    // []Entry    Entries
+  const FolderIndex({
+    required this.version,
+    required this.lastEditTimestamp,
+    required this.next,
+    required this.entries,
+  });
 
-  FolderIndex(this.version, this.lastEditTimestamp, this.next, this.entries);
+  /// uint8      Version
+  final int version;
+
+  /// uint64     Last edit timestamp
+  final int lastEditTimestamp;
+
+  /// uint64     Next
+  final int? next;
+
+  /// []Entry    Entries
+  final List<Entry> entries;
 }
 
-abstract class Entry {
-  int flags;           // uint8     Flags: [isFolder, ...reserved]
-  int indexMessageId;  // uint64    Chunk/Folder index message id
-  int nameLength;      // uint8     Name length
-  String name;         // utf-8     File/Folder name
+class Entry {
+  const Entry({
+    required this.flags,
+    required this.indexMessageId,
+    required this.nameLength,
+    required this.name,
+  });
 
-  Entry(this.flags, this.indexMessageId, this.nameLength, this.name);
+  /// uint8     Flags: [isFolder, ...reserved]
+  final int flags;
+
+  /// uint64    Chunk/Folder index message id
+  final int indexMessageId;
+
+  /// uint8     Name length
+  final int nameLength;
+
+  /// utf-8     File/Folder name
+  final String name;
 }
