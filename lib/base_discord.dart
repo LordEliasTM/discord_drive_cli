7d0b97ed7abfeb5badd3a68379887dfe06b27a82
LordElias <kerlinelias@gmail.com>
2024-01-30 15:15:39 +0100

    base256

---
+++ lib/base_discord.dart
@@ -0,0 +1,9 @@
+import 'dart:typed_data';
+
+String encodeBase256(List<int> data) {
+  return data.map((int e) => String.fromCharCode(e + 192)).join();
+}
+
+Uint8List decodeBase256(String str) {
+  return Uint8List.fromList(str.codeUnits.map((e) => e - 192).toList());
+}
