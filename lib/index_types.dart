1cf7b06387fef8732b5cc733006203c0ca455c9a
LordElias <kerlinelias@gmail.com>
2024-02-02 23:37:48 +0100

    added index tostring

--- lib/index_types.dart
+++ lib/index_types.dart
@@ -10,6 +10,18 @@ class FolderIndex {
   final int lastEdit;
   final List<FileEntry> files;
   final List<FolderEntry> folders;
+
+  @override
+  String toString() {
+    String result = "";
+    for (var folder in folders) {
+      result += "📁 ${folder.name}\n";
+    }
+    for (var file in files) {
+      result += "📄 ${file.name}\n";
+    }
+    return result;
+  }
 }
 
 class FileEntry {
