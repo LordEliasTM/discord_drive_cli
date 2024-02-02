edaa694459d54c2f6acabd6fd7c5c7a3b828d7ae
LordElias <kerlinelias@gmail.com>
2024-02-02 21:57:16 +0100

    fixing tims code :>

--- lib/byte_data_reader.dart
+++ lib/byte_data_reader.dart
@@ -2,12 +2,12 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 class ByteDataReader {
-  ByteDataReader({
-    required this.data,
-  });
+  ByteDataReader({required Uint8List data}) {
+    this.data = ByteData.sublistView(data);
+  }
 
+  late final ByteData data;
   final Utf8Decoder _utf8decoder = const Utf8Decoder();
-  final ByteData data;
   int _counter = 0;
 
   bool get finished => _counter >= data.lengthInBytes;
@@ -26,56 +26,56 @@ class ByteDataReader {
     return value;
   }
 
-  int readInt16([Endian endian = Endian.big]) {
+  int readInt16([Endian endian = Endian.little]) {
     final int value = data.getInt16(_counter, endian);
     _counter += 2;
 
     return value;
   }
 
-  int readUint16([Endian endian = Endian.big]) {
+  int readUint16([Endian endian = Endian.little]) {
     final int value = data.getUint16(_counter, endian);
     _counter += 2;
 
     return value;
   }
 
-  int readInt32([Endian endian = Endian.big]) {
+  int readInt32([Endian endian = Endian.little]) {
     final int value = data.getInt32(_counter, endian);
     _counter += 4;
 
     return value;
   }
 
-  int readUint32([Endian endian = Endian.big]) {
+  int readUint32([Endian endian = Endian.little]) {
     final int value = data.getUint32(_counter, endian);
     _counter += 4;
 
     return value;
   }
 
-  int readInt64([Endian endian = Endian.big]) {
+  int readInt64([Endian endian = Endian.little]) {
     final int value = data.getInt64(_counter, endian);
     _counter += 8;
 
     return value;
   }
 
-  int readUint64([Endian endian = Endian.big]) {
+  int readUint64([Endian endian = Endian.little]) {
     final int value = data.getUint64(_counter, endian);
     _counter += 8;
 
     return value;
   }
 
-  double readFloat32([Endian endian = Endian.big]) {
+  double readFloat32([Endian endian = Endian.little]) {
     final double value = data.getFloat32(_counter, endian);
     _counter += 4;
 
     return value;
   }
 
-  double readFloat64([Endian endian = Endian.big]) {
+  double readFloat64([Endian endian = Endian.little]) {
     final double value = data.getFloat64(_counter, endian);
     _counter += 8;
 
@@ -83,7 +83,7 @@ class ByteDataReader {
   }
 
   String readString(int length) {
-    final Uint8List view = Uint8List.sublistView(data, _counter, length);
+    final Uint8List view = Uint8List.sublistView(data, _counter, _counter + length);
     final String value = _utf8decoder.convert(view);
     _counter += length;
 
